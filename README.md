ad6bed307354411615e93e3fd4c4ecf4620dc745
ctj12461 <ctj12461@163.com>
2023-01-25 23:07:45 +0800

    Add README.md and improve help

---
+++ README.md
@@ -0,0 +1,95 @@
+## brainfuck-interpreter
+
+### Overview
+
+A fast, powerful and configurable interpreter written in Rust,
+which allows various options to meet different demends, including
+memory (tape) length configuration, EOF handling configuration and
+so on.
+
+### Usage
+
+#### Quick Start
+
+```sh
+$ git clone https://github.com/ctj12461/brainfuck-interpreter.git
+$ cd brainfuck-interpreter
+$ cargo install --path . # The path is up to you.
+$ brainfuck-interpreter ./examples/helloworld.bf
+Hello World!
+```
+
+#### Verbose Version
+
+```plain
+Usage: brainfuck-interpreter [OPTIONS] <SOURCE>
+
+Arguments:
+  <SOURCE>
+          the path of the brainfuck program source code file.
+
+Options:
+      --len <LEN>
+          the memory (tape) length the program will use.
+
+          [default: 32768]
+
+      --addr <ADDR>
+          the address range of the memory (tape).
+
+           - unsigned: [0, len - 1]
+           - signed: [-ceil(len / 2), ceil(len / 2) - 1]
+
+          [default: unsigned]
+          [possible values: unsigned, signed]
+
+      --cell <CELL>
+          the data type of one cell in the memory (tape).
+
+          [default: int8]
+          [possible values: int8, int32]
+
+      --overflow <OVERFLOW>
+          the operation the interpreter should do when an overflow error occurs.
+
+           - wrap: automatically wrap the value in cell (e.g.: `127 + 1` => `-127`)
+           - error: throw an error and abort
+
+          [default: wrap]
+          [possible values: wrap, error]
+
+      --eof <EOF>
+          the operation the interpreter should do when an `EOF` is read.
+
+           - zero: turn `EOF` to `0`
+           - keep: keep what the `EOF` is and return it (`EOF == -1`)
+           - ignore: ignore this input and leave the cell unchanged
+
+          [default: ignore]
+          [possible values: zero, keep, ignore]
+
+      --input <INPUT>
+          the input stream type.
+
+          [default: std]
+          [possible values: null, std]
+
+      --output <OUTPUT>
+          the output stream type.
+
+          [default: char-std]
+          [possible values: char-std, int-std]
+
+  -h, --help
+          Print help (see a summary with '-h')
+
+  -V, --version
+          Print version
+```
+
+### License
+
+Licensed under MIT.
+
+Copyright (C) Justin Chen (ctj12461), 2023
+
--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@
 //! which allows various options to meet different demends, including
 //! memory (tape) length configuration, EOF handling configuration and
 //! so on.
-//! 
+//!
 //! Licensed under MIT.
 //! Copyright (C) Justin Chen (ctj12461), 2023
-//! 
+//!
 
 #![allow(unused)]
 
-use std::path::PathBuf;
 use std::error::Error;
+use std::path::PathBuf;
 
 use brainfuck_interpreter::{
-    Interpreter, MemoryConfig, StreamConfig, memory_config, stream_config
+    memory_config, stream_config, Interpreter, MemoryConfig, StreamConfig,
 };
 use clap::{builder::PathBufValueParser, command, value_parser, Arg};
 
@@ -28,58 +28,104 @@ fn main() -> Result<(), Box<dyn Error>> {
 fn input() -> clap::ArgMatches {
     let cmd = command!();
     let cmd = cmd.arg(
-        Arg::new("len")
+        Arg::new("LEN")
             .long("len")
             .required(false)
             .value_parser(value_parser!(usize))
-            .default_value("32768"),
+            .default_value("32768")
+            .next_line_help(true)
+            .help("the memory (tape) length the program will use.\n")
+            .long_help("the memory (tape) length the program will use."),
     );
     let cmd = cmd.arg(
-        Arg::new("addr")
+        Arg::new("ADDR")
             .long("addr")
             .required(false)
             .value_parser(["unsigned", "signed"])
-            .default_value("unsigned"),
+            .default_value("unsigned")
+            .next_line_help(true)
+            .help("the address range of the memory (tape).\n")
+            .long_help({
+                let mut h = String::new();
+                h.push_str("the address range of the memory (tape).\n");
+                h.push_str("\n");
+                h.push_str(" - unsigned: [0, len - 1]\n");
+                h.push_str(" - signed: [-ceil(len / 2), ceil(len / 2) - 1]");
+                h
+            }),
     );
     let cmd = cmd.arg(
-        Arg::new("cell")
+        Arg::new("CELL")
             .long("cell")
             .required(false)
             .value_parser(["int8", "int32"])
-            .default_value("int8"),
+            .default_value("int8")
+            .next_line_help(true)
+            .help("the data type of one cell in the memory (tape).\n")
+            .long_help("the data type of one cell in the memory (tape)."),
     );
     let cmd = cmd.arg(
-        Arg::new("overflow")
+        Arg::new("OVERFLOW")
             .long("overflow")
             .required(false)
             .value_parser(["wrap", "error"])
-            .default_value("wrap"),
+            .default_value("wrap")
+            .next_line_help(true)
+            .help("the operation the interpreter should do when an overflow error occurs.\n")
+            .long_help({
+                let mut h = String::new();
+                h.push_str("the operation the interpreter should do when an overflow error occurs.\n");
+                h.push_str("\n");
+                h.push_str(" - wrap: automatically wrap the value in cell (e.g.: `127 + 1` => `-127`)\n");
+                h.push_str(" - error: throw an error and abort");
+                h
+            }),
     );
     let cmd = cmd.arg(
-        Arg::new("eof")
+        Arg::new("EOF")
             .long("eof")
             .required(false)
             .value_parser(["zero", "keep", "ignore"])
-            .default_value("ignore"),
+            .default_value("ignore")
+            .next_line_help(true)
+            .help("the operation the interpreter should do when an `EOF` is read.\n")
+            .long_help({
+                let mut h = String::new();
+                h.push_str("the operation the interpreter should do when an `EOF` is read.\n");
+                h.push_str("\n");
+                h.push_str(" - zero: turn `EOF` to `0`\n");
+                h.push_str(" - keep: keep what the `EOF` is and return it (`EOF == -1`)\n");
+                h.push_str(" - ignore: ignore this input and leave the cell unchanged");
+                h
+            })
     );
     let cmd = cmd.arg(
-        Arg::new("input")
+        Arg::new("INPUT")
             .long("input")
             .required(false)
             .value_parser(["null", "std"])
-            .default_value("std"),
+            .default_value("std")
+            .next_line_help(true)
+            .help("the input stream type.\n")
+            .long_help("the input stream type."),
     );
     let cmd = cmd.arg(
-        Arg::new("output")
+        Arg::new("OUTPUT")
             .long("output")
             .required(false)
             .value_parser(["char-std", "int-std"])
-            .default_value("char-std"),
+            .default_value("char-std")
+            .next_line_help(true)
+            .help("the output stream type.\n")
+            .long_help("the output stream type."),
     );
     let cmd = cmd.arg(
-        Arg::new("source")
+        Arg::new("SOURCE")
             .required(true)
-            .value_parser(PathBufValueParser::new()),
+            .value_parser(PathBufValueParser::new())
+            .next_line_help(true)
+            .help("the path of the brainfuck program source code file.\n")
+            .long_help("the path of the brainfuck program source code file."),
     );
 
     let matches = cmd.get_matches();
@@ -88,23 +134,23 @@ fn input() -> clap::ArgMatches {
 
 fn parse<'a>(matches: &'a clap::ArgMatches) -> (MemoryConfig, StreamConfig, &'a PathBuf) {
     let memory_config = MemoryConfig {
-        len: *matches.get_one::<usize>("len").unwrap(),
-        addr: match matches.get_one::<String>("addr").unwrap().as_str() {
+        len: *matches.get_one::<usize>("LEN").unwrap(),
+        addr: match matches.get_one::<String>("ADDR").unwrap().as_str() {
             "unsigned" => memory_config::Addr::Unsigned,
             "signed" => memory_config::Addr::Unsigned,
             _ => unreachable!(),
         },
-        cell: match matches.get_one::<String>("cell").unwrap().as_str() {
+        cell: match matches.get_one::<String>("CELL").unwrap().as_str() {
             "int8" => memory_config::Cell::I8,
             "int32" => memory_config::Cell::I32,
             _ => unreachable!(),
         },
-        overflow: match matches.get_one::<String>("overflow").unwrap().as_str() {
+        overflow: match matches.get_one::<String>("OVERFLOW").unwrap().as_str() {
             "wrap" => memory_config::Overflow::Wrap,
             "error" => memory_config::Overflow::Error,
             _ => unreachable!(),
         },
-        eof: match matches.get_one::<String>("eof").unwrap().as_str() {
+        eof: match matches.get_one::<String>("EOF").unwrap().as_str() {
             "zero" => memory_config::Eof::Zero,
             "keep" => memory_config::Eof::Keep,
             "ignore" => memory_config::Eof::Ignore,
@@ -113,19 +159,19 @@ fn parse<'a>(matches: &'a clap::ArgMatches) -> (MemoryConfig, StreamConfig, &'a 
     };
 
     let stream_config = StreamConfig {
-        input: match matches.get_one::<String>("input").unwrap().as_str() {
+        input: match matches.get_one::<String>("INPUT").unwrap().as_str() {
             "null" => stream_config::Input::Null,
             "std" => stream_config::Input::Standard,
             _ => unreachable!(),
         },
-        output: match matches.get_one::<String>("output").unwrap().as_str() {
+        output: match matches.get_one::<String>("OUTPUT").unwrap().as_str() {
             "char-std" => stream_config::Output::CharStandard,
             "int-std" => stream_config::Output::IntStandard,
             _ => unreachable!(),
         },
     };
 
-    let source = matches.get_one::<PathBuf>("source").unwrap();
+    let source = matches.get_one::<PathBuf>("SOURCE").unwrap();
     (memory_config, stream_config, source)
 }
 
